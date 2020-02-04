e566a4dd93208e46a5674c0744a7d7ba4d78789d
Sanchit Oza <sanchithoza@gmail.com>
2020-02-04 17:41:24 +0530

    statred real todo app

---
+++ server/server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose.js');
+var {Todo} = require('./models/todo.js');
+var {User} = require('./models/user.js');
+
+var port = 3000;
+var app = express();
+
+app.use(bodyParser.json());
+app.post('/todos',(req,res)=>{
+  var todo = new Todo({
+    text:req.body.text
+  });
+  todo.save().then((doc)=>{
+    res.send(doc);
+  },(err)=>{
+    res.status(400).send(err);
+  })
+});
+
+app.listen(port,()=>{
+  console.log('started on port :',port);
+})
