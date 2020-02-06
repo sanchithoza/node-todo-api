4201464843f8327901f04725f796d75b05bf266a
Sanchit Oza <sanchithoza@gmail.com>
2020-02-06 09:42:45 +0530

    get /todos test added

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -5,11 +5,17 @@ const request = require('supertest');
 var {app} = require('./../server.js');
 var {Todo} = require('./../models/todo.js');
 
-
+const todos = [{
+  text: 'first test todo'
+},{
+  text: 'second test todo'
+}];
 
 describe('post /todos',()=> {
   beforeEach((done)=>{
-    Todo.deleteMany({}).then(()=>done());
+    Todo.deleteMany({}).then(()=>{
+      Todo.insertMany(todos);
+    }).then(()=>done());
   });
 
   it('should create a new todo',(done)=>{
@@ -26,7 +32,7 @@ describe('post /todos',()=> {
         if(err){
           return done(err);
         }
-        Todo.find().then((todos)=>{
+        Todo.find({text}).then((todos)=>{
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -43,9 +49,22 @@ describe('post /todos',()=> {
         return done(err);
       }
       Todo.find().then((todos)=>{
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done();
       }).catch((err)=>done(err));
     });
   });
+});
+
+describe('GET /todos',()=>{
+  it('should get all todos',(done)=>{
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done);
+  });
+
 });
