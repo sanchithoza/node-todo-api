51b5284e9aa51ca2ae80af59e8ba913aa554e2b3
Sanchit Oza <sanchithoza@gmail.com>
2020-02-06 16:31:12 +0530

    reviving app heroku issue

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Todoapp',{useNewUrlParser: true,useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/Todoapp',{useNewUrlParser: true,useUnifiedTopology: true});
 
 module.exports={
   mongoose
