fcc31542d090de7f271dff1d6c4a18836188c825
Sanchit Oza <sanchithoza@gmail.com>
2020-02-06 11:07:26 +0530

    added get /todo/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectId} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
@@ -36,7 +36,21 @@ app.get('/todos',(req,res)=>{
     res.status(400).send(err);
   })
 });
+app.get('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  if(!ObjectId.isValid(id)){
+    return res.status(404).send('invalid id');
+  }
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.status(200).send({todo});
+  },(e)=>{
+    res.status(404).send();
+  });
 
+});
 
 app.listen(port,()=>{
   console.log('started on port :',port);
